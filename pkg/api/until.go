cb3ad1d6c9b7c92e0d2fb5933cf151321c0701ae
Hiroki Matsumoto <hiroki.b.matsumoto@rakuten.com>
2021-01-18 10:06:32 +0900

    add until handler

--- pkg/api/until.go
+++ pkg/api/until.go
@@ -20,7 +20,7 @@ import (
 func (s *Server) untilHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
-	until, err := time.Parse("225630",vars["until"])
+	until, err := time.Parse("150405",vars["until"])
 	if err != nil {
 		s.ErrorResponse(w, r, err.Error(), http.StatusBadRequest)
 		return
